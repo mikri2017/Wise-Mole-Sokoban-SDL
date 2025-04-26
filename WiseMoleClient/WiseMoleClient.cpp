cedd0028763e8490c8a7a323d8678e38b29af718
MIKRI <2007mik007@mail.ru>
2025-04-26 14:27:36 +0700

    Добавил начальный созданный Visual Studio 2022 проект CMake и описание проекта в файле REDME.md

---
+++ WiseMoleClient/WiseMoleClient.cpp
@@ -0,0 +1,12 @@
+﻿// WiseMoleClient.cpp: определяет точку входа для приложения.
+//
+
+#include "WiseMoleClient.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
