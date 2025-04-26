44b7d78ae339d3df47f59de0948e05244b797434
MIKRI <2007mik007@mail.ru>
2025-04-26 19:35:30 +0700

    Заменил CMake на решение и проекты Visual Studio

---
+++ WiseMoleClient/main.cpp
@@ -0,0 +1,47 @@
+#define SDL_MAIN_USE_CALLBACKS 1  // Использовать обратные вызовы, вместо main()
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+// Будем использовать этот renderer для рисования в этом окне каждого кадра
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
+// Эта функция отрабатывает при запуске, один раз
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    SDL_SetAppMetadata("WiseMole Sokoban", "0.1", "com.mikri.GAMES");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("WiseMole Sokoban", 1024, 768, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE; // продолжим выполнение программы
+}
+
+// Эта функция отрабатывает при новом событии (ввод мыши, нажатие клавиши, и т.д...
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS; // завершение программы, сообщение ОС об успехе
+    }
+
+    return SDL_APP_CONTINUE; // продолжим выполнение программы
+}
+
+// Эта функция запускается каждый кадр - сердце программы
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    return SDL_APP_CONTINUE; // продолжим выполнение программы
+}
+
+// Эта функция запускается при завершении программы
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    // SDL освободит окно, рендер для нас
+}
